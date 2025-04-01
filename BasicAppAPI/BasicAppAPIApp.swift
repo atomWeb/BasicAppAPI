3423b69ed9898a70b6b7d9c570655f14749a09ea
nabusim <cristianjose@gmail.com>
2025-04-01 21:08:31 +0200

    Initial Commit

---
+++ BasicAppAPI/BasicAppAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicAppAPIApp.swift
+//  BasicAppAPI
+//
+//  Created by Cristian Jose Perez Guerra on 1/4/25.
+//
+
+import SwiftUI
+
+@main
+struct BasicAppAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasicAppAPI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BasicAppAPI
+//
+//  Created by Cristian Jose Perez Guerra on 1/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
